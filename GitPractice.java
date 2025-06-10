694b1a490649a07cb58b1b7ebb33aa99961985d9
Masashi Yoshida <masashi4406@gmail.com>
2025-06-10 11:10:14 +0900

    再度作業済み

--- GitPractice.java
+++ GitPractice.java
@@ -3,5 +3,7 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("push from masashi's branch");
+
+        System.out.println("再度作業");
     }
 }
